a2f44d40c023e031d63a99f398400cec9f649c75
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-06-10 01:05:49 +0500

    comments added

--- Picture Box/Form1.cs
+++ Picture Box/Form1.cs
@@ -24,14 +24,19 @@ namespace Picture_Box
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // create an instance of OpenFileDialog
             OpenFileDialog openFileDialog = new OpenFileDialog();
 
+            // set the filter to allow image files with various extensions
             openFileDialog.Filter = "Image Files | *jpg; *jpeg; *png; *gif; *bmp; *.tiff";
 
+            // display the OpenFileDialog and check if the user selected a file
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
+                // get the selected file path
                 string imagePath = openFileDialog.FileName;
 
+                // load the image file into the PictureBox control
                 pictureBox1.Image = Image.FromFile(imagePath);
             }
         }
